a4f298c4c5e26d47d8ed2f388ecb3a3030d3de81
EvertonSouzaFerreira <everton.souzaferreira@outlook.com>
2022-09-06 21:01:16 +0200

    condicionais1

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt('qual ');
+let isEnsinoMedio = confirm('Terminou o ensino medio');
+let isCursandoOutraFaculdade = confirm('Terminou o a faculdade');
 
-if(){
-
-}else{
-	
+if (idade >= 18) {
+  console.log("Voce e maior de idade");
+} else {
+  console.log("voce e menor de idade");
 }
+
+if (isEnsinoMedio) {
+  console.log("Voce terminou o ensino medio");
+} else {
+  console.log("voce nao terminor o ensino medio");
+}
+
+if (isCursandoOutraFaculdade) {
+        console.log("Voce esta cursando uma faculdade");
+      } else {
+        console.log("Voce nao esta cursando uma faculdade");
+      }
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,19 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt
+('Qual a sua nacionalidade? nosso programa so aceita as seguintes nacionalidades brasileira , argentina, uruguaia, chilena, colombiana').toLowerCase().trim()
+
+
+
+if (nacionalidade === 'brasileira'){
+    console.log('Voce e Brasileiro')
+}else if(nacionalidade === 'argentina'){
+    console.log('Voce e Argentino')
+}else if(nacionalidade === 'uruguaia'){
+    console.log('Voce e Uruguaio')
+}else if(nacionalidade === 'chilena'){
+    console.log('Voce e Chileno')
+}else if(nacionalidade === 'colombiana'){
+    console.log('Voce e Colombiano')
+}else{
+    console.log('informacoes nao encontradas')
+}
+
